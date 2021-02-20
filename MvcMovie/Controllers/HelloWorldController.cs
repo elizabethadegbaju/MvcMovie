167affb6d45de47ee2d3396ada1741e38e27b2fe
Elizabeth Adegbaju <adeotunadegbaju@gmail.com>
2021-02-20 22:27:17 +0100

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is the welcome default action...";
+        }
+        public string Welcome()
+        {
+            return "This is the welcome action method!";
+        }
+    }
+}
