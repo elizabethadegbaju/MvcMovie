f6ff27990149106f0482d3160eb938702899efe3
Elizabeth Adegbaju <adeotunadegbaju@gmail.com>
2021-02-21 22:18:37 +0100

    fix: add data annotations for validations

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -10,15 +10,20 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [StringLength(65, MinimumLength = 3)]
         public string Title { get; set; }
 
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-mm-yyyy}", ApplyFormatInEditMode = true), Display(Name = "Release Date"), DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$"), StringLength(30), Required]
         public string Genre { get; set; }
 
-        [Column(TypeName = "decimal(18,2)")]
+        [Range(1, 100), DataType(DataType.Currency), Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5), Required]
         public string Rating { get; set; }
     }
 }
